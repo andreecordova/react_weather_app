e3764263da0590f90b14d539f8e48ec5088f055b
Andree Fabian Cordova Luna <andreefabian.cordova@gmail.com>
2021-01-26 18:22:43 -0500

    refactor():Iniciando avance en proyecto sobre busqueda de clima

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import keys from "./variables/keys";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
+  const dateBuild = (d) => {
+    let [date, time] = d.toLocaleString('es-PE').split(' ');
+    return `Fecha: ${date} Hora: ${time}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 mb-3">
+          <h1>React Weather App</h1>
+        </div>
+        <div className="col-12">
+          <p><strong>{dateBuild(new Date())}</strong></p>
+        </div>
+        <div className="col-12">
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" placeholder="Search" aria-label="Weather search" aria-describedby="search-box" />
+            <div className="input-group-append">
+              <span className="input-group-text" id="search-box">
+                <FontAwesomeIcon icon={faSearch} />
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+    
   );
 }
 
